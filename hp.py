de0e345b7efc0b24af53712cd3d57c9f8f65c24a
Kambus <kambus@gmail.com>
2010-04-08 17:47:23 +0200

    version 0.2

--- hp.py
+++ hp.py
@@ -1,14 +1,16 @@
+#!/usr/bin/env python
+
 from lxml.html import fromstring, tostring
 from urllib2 import urlopen
-import os, re
+import sys, os, re
 import curses
 import tempfile, zipfile
 
-cim = '4400'
-# if sys.argv[1]:
-#     cim = sys.argv[1]
-# else:
-#     cim = raw_input('cim: ')
+if sys.argv[1]:
+    cim = ' '.join(sys.argv[1:])
+else:
+    cim = raw_input('cim: ')
+
 url = 'http://www.hosszupuskasub.com/index.php?cim=%s' % cim
 subs = []
 row = 0
@@ -25,18 +27,19 @@ html = fromstring(c)
 class Win:
     def __init__(self, scr):
         self.scr = scr
+        self.scr.clear()
         self.list = []
         self.maxy, self.maxx = self.scr.getmaxyx()
-        self.scr.clear()
-        self.row = 0
+        self.row = 0        # current row
+        self.idx = 0        # current sub
         self.page = 0
         self.len = 0
-        self.y = 0
-        self.x = 0
+        self.y = 0          # top border
+        self.x = 0          # left border
 
     def printsubs(self):
         lrow = 0
-        idx = self.page * self.maxy + self.row
+        self.updidx()
         downl = self.page * self.maxy
         upl = (self.page + 1) * self.maxy
 
@@ -45,18 +48,18 @@ class Win:
             lrow += 1
 
         self.scr.addstr(self.y + self.row, self.x,
-                self.list[idx], curses.A_STANDOUT)
+                self.list[self.idx], curses.A_STANDOUT)
         self.scr.refresh()
 
     def move(self, i):
-        idx = self.page * self.maxy + self.row
+        self.updidx()
         maxrow = self.len - self.page * self.maxy - 1
 
         if i >= 0 and i <= maxrow and i < self.maxy:
-            self.scr.addstr(self.y + self.row, self.x, self.list[idx])
+            self.scr.addstr(self.y + self.row, self.x, self.list[self.idx])
             self.row = i
-            idx = self.page * self.maxy + self.row
-            self.scr.addstr(self.y + self.row, self.x, self.list[idx],
+            self.updidx()
+            self.scr.addstr(self.y + self.row, self.x, self.list[self.idx],
                     curses.A_STANDOUT)
         elif i >= self.maxy:
             self.page += 1
@@ -68,6 +71,10 @@ class Win:
             self.scr.erase()
             self.row = self.maxy-1
             self.printsubs()
+        self.updidx()
+
+    def updidx(self):
+        self.idx = self.page * self.maxy + self.row
 
 class Root(Win):
     def __init__(self, scr):
@@ -77,6 +84,7 @@ class Root(Win):
         self.maxy, self.maxx = self.scr.getmaxyx()
         self.scr.clear()
         self.row = 0
+        self.idx = 0
         self.page = 0
         self.len = 0
         self.y = 0
@@ -91,6 +99,7 @@ class SubWin(Win):
         self.maxy -= 1
         self.scr.clear()
         self.row = 0
+        self.idx = 0
         self.page = 0
         self.len = len(self.list)
         self.y = 1
@@ -99,37 +108,30 @@ class SubWin(Win):
         self.scr.box()
         self.scr.addstr(0, 3, 'download')
 
-root = Root(scr)
-
-for a in html.cssselect('a#menu'):
-    if 'download.php' in a.get('href'):
-        root.list.append(' - '.join((
-            re.search('<br>(.+?)<\/td>', tostring(a.getparent())).group(1),
-            a.getparent().getnext().getchildren()[0].attrib['alt'])))
-        root.dl.append(a.get('href'))
-        root.len += 1
-
-root.printsubs()
-
-
 def fetch():
-    link = 'http://www.hosszupuskasub.com/' + root.dl[root.row]
+    link = 'http://www.hosszupuskasub.com/' + root.dl[root.idx]
     f = tempfile.NamedTemporaryFile()
     f.write(urlopen(link).read())
     zf = None
     files = []
-    if re.search('\.zip$', root.dl[root.row]):
+    isZip = False
+    if re.search('\.zip$', root.dl[root.idx]):
         zf = zipfile.ZipFile(f)
         files = zf.namelist()
-    elif re.search('\.rar$', root.dl[root.row]):
-        files = os.popen('unrar lb %s' % f.name).read().split('\n')
+        isZip = True
+    elif re.search('\.rar$', root.dl[root.idx]):
+        files = os.popen('unrar v %s | awk \'BEGIN { s=0; f=1; } \
+                /^---/ {s++; if(s>1) exit 0; next; } \
+                s==1 && f++ %% 2 { sub("^ ", ""); print; }\'' % f.name).read().split('\n')
+#         files = os.popen('unrar lb %s' % f.name).read().split('\n')
         files.pop()
 
-    submaxx = submaxx = len(files) + 2
+    submaxy = submaxy = len(files) + 2
     if len(files)+2 > root.maxy:
-        submaxx = root.maxy - 2
+        submaxy = root.maxy - 2
     dwin = SubWin(
-            root.scr.subwin(submaxx, int(root.maxx * 0.85), 10, 2), files)
+            root.scr.subwin(submaxy, int(root.maxx * 0.85), 10, 2), files)
+    dwin.scr.addstr(0, 3, re.search(r'file=(.*$)', root.dl[root.idx]).group(1))
     dwin.printsubs()
 
     while True:
@@ -149,8 +151,16 @@ def fetch():
                 dwin.move(dwin.maxy - 1)
             else:
                 dwin.move(dwin.len-1 - dwin.page*dwin.maxy)
-#         elif c in (ord('e'), curses.KEY_ENTER, 10):
-#             fetch()
+        elif c in (ord('e'), ord('s'), curses.KEY_ENTER, 10):
+            if isZip:
+                zf.extract(files[dwin.maxy*dwin.page + dwin.row])
+            else:
+                os.system('unrar e -inul %s "%s"' % (f.name, files[dwin.idx]))
+        elif c in (ord('a'), ord('A')):
+            if isZip:
+                zf.extractall()
+            else:
+                os.system('unrar e -inul %s' % f.name)
         elif c in (ord('r'), ord('R')):
             dwin.scr.refresh()
     dwin.scr.erase()
@@ -166,6 +176,22 @@ def end():
     curses.echo()
     curses.endwin()
 
+root = Root(scr)
+
+for a in html.cssselect('a#menu'):
+    if 'download.php' in a.get('href'):
+        root.list.append(' - '.join((
+            re.search('<br>(.+?)<\/td>', tostring(a.getparent().getparent().getchildren()[1])).group(1),
+            a.getparent().getparent().getchildren()[2].find('img').attrib['alt'])))
+        root.dl.append(a.get('href'))
+        root.len += 1
+
+if root.list:
+    root.printsubs()
+else:
+    end()
+    sys.exit()
+
 while True:
     c = scr.getch()
     if c == ord('q'):
@@ -178,21 +204,12 @@ while True:
     elif c in (ord('g'), curses.KEY_HOME):
         root.move(0)
     elif c in (ord('G'), curses.KEY_END):
-        if root.len - 1 < root.maxy:
-            root.move(root.len - 1)
+        if root.len - 1 < root.maxy:    # ha kisebb mint az ablak
+            root.move(root.len - 1)     # a vegere ugrunk
         elif root.len - 1 > (root.page+1) * root.maxy:
-            root.move(root.maxy - 1)
-        else:
+            root.move(root.maxy - 1)    # ha nagyobb az akkor az ablak aljara
+        else:   # utolso oldal miatt kell
             root.move(root.len-1 - root.page*root.maxy)
-#     elif c in (ord('i'), ord('I')):
-#         subscr = scr.subwin(5, 65, 10, 2)
-#         subscr.addstr(1, 1, '; '.join(subs[row]), curses.A_STANDOUT)
-#         subscr.box()
-#         subscr.addstr(0, 3, 'info box')
-#         subscr.getch()
-#         subscr.erase()
-#         printsubs()
-#         scr.addstr(row, 0, subs[row][0], curses.A_STANDOUT)
     elif c in (ord('f'), curses.KEY_ENTER, 10):
         fetch()
     elif c in (ord('r'), ord('R')):
